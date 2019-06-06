4557dfb8730e75b89c9bb3e508fcaeba2d06e413
xndy930 <im.dengcm@gmail.com>
2019-06-06 19:08:46 +0800

    2048 game

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <ul id="wrap">
+      <span class="scort">总分：{{scort}}</span>
+      <li class='box'
+          v-for="num in nums"
+          v-text="num"
+          v-getclass="num"
+          track-by="$index">
+      </li>
+    </ul>
+
+    <div class="mask" v-show='show===1' @click='show=0' transition='show'>
+      <div>{{msg}}</div>
+    </div>
+    <button @click="reset">重置</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      scort : 0,
+      show : 0,
+      msg : '',
+      nums : [],
+
+    }
+  },
+  mounted() {
+  }
+
+
+}
+</script>
+
+<style>
+  @import url(./main.css);
+</style>
